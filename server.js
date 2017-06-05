a02188ad5ce30c6cda493424684d7e36f3640fd0
Andrés Sarmiento <Sarmiento@amatea.org>
2017-06-05 15:18:43 -0500

    refactor view server n sass

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(session({
 }));
 
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const api = require('./routes');
 
