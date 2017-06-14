9b5b684b85fa4b03ce71f5a0ed43dcd18cb8419e
Andrés Sarmiento <Sarmiento@amatea.org>
2017-06-14 18:52:32 -0500

    data test

--- README.md
+++ README.md
@@ -9,5 +9,18 @@ npm install
 ## Iniciar App
 ```
 gulp
-```
 node server
+
+```
+
+## Data Test
+```
+Desde POSTMAN
+
+Hacer una peticion POST
+http://localhost:3000/admin/signup
+
+key: cedula,    value: Any Number
+key: password,  value: Any
+key: role,      value: admin || juez
+```
--- models/UserAdmin.js
+++ models/UserAdmin.js
@@ -37,15 +37,4 @@ useradminSchema.methods.comparePassword = function comparePassword(candidatePass
   });
 };
 
-useradminSchema.methods.gravatar = function gravatar(size) {
-  if (!size) {
-    size = 200;
-  }
-  if (!this.email) {
-    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-  }
-  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
-  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
-};
-
 module.exports = mongoose.model('useradmin', useradminSchema);
